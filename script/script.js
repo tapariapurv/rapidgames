d3d302f5fd612694095c35de8085786f420aec14
Purv Taparia <tapariapurv@gmail.com>
2025-09-30 18:53:57 +0530

    Removed rating system and google API keys

--- script/script.js
+++
@@ -1,8 +0,0 @@
-function rateGame(el, rating) {
-    let stars = el.parentNode.querySelectorAll("span");
-    stars.forEach((star, index) => {
-      star.style.color = (5 - index) <= rating ? "gold" : "gray";
-    });
-    document.getElementById("rating-result").innerText = "You rated this game " + rating + " stars!";
-  }
-  
