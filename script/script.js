db027a2fe3a3434c2c2cb81fc675ed54dace89c2
Purv Taparia <tapariapurv@gmail.com>
2025-10-19 10:35:28 +0530

    Added Chatbase history

--- script/script.js
+++ script/script.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const gameItems = document.querySelectorAll('.game-item');
+    let currentCategory = 'all';
 
-    searchInput.addEventListener('keyup', function(event) {
-        const searchQuery = event.target.value.toLowerCase();
+    function filterGames() {
+        const searchQuery = searchInput.value.toLowerCase();
 
         gameItems.forEach(function(game) {
             const gameName = game.dataset.name.toLowerCase();
+
             if (gameName.includes(searchQuery)) {
                 game.style.display = '';
             } else {
                 game.style.display = 'none';
             }
         });
-    });
+    }
+
+    searchInput.addEventListener('keyup', filterGames);
+
 });
